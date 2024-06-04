af1e65c1c886d1d9849b6ebec5a85c2c20f76e9a
resoni-majesha <77217620+resoni-majesha@users.noreply.github.com>
2024-06-04 12:47:26 +0530

    Add files via upload

---
+++ day224/LearnOperators.java
@@ -0,0 +1,39 @@
+package week1.day224;
+
+public class LearnOperators {
+
+	public static void main(String[] args) {
+		
+		int a = 2;
+        
+		System.out.println(5+6);
+		System.out.println(10%3);
+		System.out.println(5>4);
+		System.out.println(2<1);
+		System.out.println(a+=6); // a = 2+6
+		System.out.println(a==2 && a!=1);
+		System.out.println(a==2 || a!=1);
+
+	}
+
+}
+
+/*
+11
+1
+true
+false
+8
+false
+true
+*/
+
+
+ /*
+ * Operators - Perform specific condition
+ * Arithmetic operators - +, -, *, /, %
+ * Assignment operators - +=, -=, *=, /=, %=
+ * Comparison operators - ==, !=, <, <=, >, >=
+ * Logical Operators - &&, ||
+ * Unary operator - i++, i--
+*/

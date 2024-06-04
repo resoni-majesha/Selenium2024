eef6735df977a2fe501b4f34ce1eeab2c3d55a66
resoni-majesha <77217620+resoni-majesha@users.noreply.github.com>
2024-06-04 12:44:38 +0530

    Add files via upload

---
+++ day124/SeleniumJavaIndex.java
@@ -0,0 +1,22 @@
+package week1.day124;
+
+public class SeleniumJavaIndex {
+
+	/*
+	 * Week 1 Day 1
+	 * Basics of Java
+	 * First Program
+	 * Datatypes
+	 * Control Statements
+	 * 
+	 * Week 1 Day 2
+	 * Loops
+	 * Print Odd/Even Numbers
+	 * Operators
+	 * Objects
+	 * Methods
+	 * Arrays
+	 * Print duplicates
+	 */
+	
+}

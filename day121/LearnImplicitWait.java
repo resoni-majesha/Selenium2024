acbb6802b9e1d8964e6e262c6468c149099f29ca
resoni-majesha <77217620+resoni-majesha@users.noreply.github.com>
2024-06-04 12:50:00 +0530

    Add files via upload

---
+++ day121/LearnImplicitWait.java
@@ -0,0 +1,78 @@
+package week2.day121;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LearnImplicitWait {
+
+	public static void main(String[] args) {
+		
+        ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		
+		// Set up the implicitly wait
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		
+		driver.manage().window().maximize();
+		
+		String title = driver.getTitle();
+		System.out.println(title);
+		
+		driver.findElement(By.id("username1")).sendKeys("demosalesmanager");
+		
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		
+		driver.findElement(By.className("decorativeSubmit")).click();
+		
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		
+		driver.findElement(By.linkText("Create Lead")).click();
+		
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Amazon");
+		
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Resoni X");
+		
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Majesha");
+		
+		driver.findElement(By.name("submitButton")).click();      
+		
+		driver.close();
+		
+	}
+
+}
+
+/*
+ * Waits
+ * There are 2 different waits in Selenium 
+ * 1. Implicit wait
+ * 2. Explicit wait
+ * 
+ * Why do we need to use wait
+ * Is your office website loads faster all the time?
+ * While doing manually, yu will wait or login a defect for page loading? Wait for loading
+ * Yu can wait for 5 secs, yu can wait
+ * If the delay is always happens, yu can raise a defect
+ * 
+ * Maximum wait time -> Finding the bus to arrive at your place
+ * Your frnd is also waiting for the same bus in next busstep
+ * You both wait for 15 mins if the bus arrives late
+ * 15 mins is the max time
+ * 
+ * All the passengers wait for the max time of 15 mins
+ * If the bus arrives within 15 mins, what will yu do, board the bus
+ * If the wait time is beyond 15 mins, we will decide what's next
+ * 
+ * This approach is called implicit wait
+ * Implicit wait keeps on waiting for the elemnet to appear on the webpage for a max time provided
+ * If it exceeds max time, it will throw an exception -> I couldn't find the element
+ * 
+ * Whenever Selenium could not find the element within the timeout -> It throws NoSuchElementException
+ * NoSuchElementException is a Java Class
+ * If the username element is provided right, then the code will work fine immediately
+ */
---
+++ day121/Login.java
@@ -0,0 +1,141 @@
+package week2.day121;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+
+	public static void main(String[] args) {
+		
+		// Step 1: Ask Webdrivermanager to assist you download the chromedriver if it is not present in your machine
+		
+		// Step 2: Launch the chrome browser -> Let Java code to launch it
+		
+		ChromeDriver driver = new ChromeDriver();
+		
+		// Firefox Driver = new FirefoxDriver(); // Launch firefox driver
+		
+		// Step 3: Load the URL of the application
+		
+		// http://leaftaps.com/opentaps/control/main // demosalesmanager / crmsfa
+		
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		
+		// Step 4: Maximize the browser
+		
+		driver.manage().window().maximize();
+		
+		// Print the title
+		
+		String title = driver.getTitle();
+		System.out.println(title);
+		
+		// Step 5: Enter the username
+		// id -> username
+		
+		driver.findElement(By.id("username")).sendKeys("demosalesmanager", Keys.TAB); 
+		
+		//Keys.TAB is used to enter text and tap on the particular element
+		//eg: In redbus app, we need to type and select the location, for that use Keys.TAB
+		
+		// clear() is used to enter value and erase
+		
+		// Step 6: Enter the password
+		// id -> password
+		
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		
+		// Step 7: Click on Login button
+		// className -> decorativeSubmit
+		
+		driver.findElement(By.className("decorativeSubmit")).click();
+		
+		// Step 8: Click on crmsfa link
+		
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		
+		// Step 9: Click Create Lead link
+		
+		driver.findElement(By.linkText("Create Lead")).click();
+		
+		// Step 10: Type your company name
+		
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Amazon");
+		
+		// Step 11: Type your first name
+		
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Resoni X");
+		
+		// Step 12: Type your last name
+		
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Majesha");
+		
+		// Step 13: Click create Lead
+		
+		driver.findElement(By.name("submitButton")).click();
+		
+		// Print the title of the loaded page
+		
+		String title2 = driver.getTitle();
+		
+		System.out.println(title2);
+		
+		driver.close();
+	}
+
+}
+
+/*
+ * Selenium Webdriver cannot communicate with the web browser which is already launched
+ * It can only talk to the new browser
+ * 
+ * The browser enders the application code as HTML
+ * HTML Tags - Tags are used to communicate to the devs/ users to tell what is that element is all about
+ * 
+ * <html> -> ROOT tag -> HTML Page
+ * <input> -> Input Field -> textbox, button, checkbox, radio button
+ * <button> -> button
+ * <div> -> divide the content into multiple portions
+ * <a> -> Hyperlink
+ * <table> 
+ * <span>
+ * 
+ * Element will contain 3 information other than tag
+ * 
+ * <input name ="Maje" age = "27" org = "TestLeaf"
+ * 
+ * 1. Element Attribute - name, age, org - Within the tag - Pink
+ * 2. Element Attribute's value - Maje, 27, TestLeaf - followed by the attribute - Blue
+ * 3. Element Text - In between opening & closing tag <input> Maje </input> - Black
+ * 
+ * All the above 3 info's are not mandatory, use may/may not have, but the tag is mandatory
+ * 
+ * To know the Attribute, Attribute's value, Text, go to developer tools by clicking on F12
+ * Other approach, right click on the element & inspect element
+ * 
+ * Selenium Locators - 8
+ * 
+ * 1. id - attribute
+ * 2. name - attribute
+ * 3. className - attribute
+ * 4. tagName - tag
+ * 5. linkText - text of the tag <a> - Exact match
+ * 6. partialLinkText - partial text of the tag <a> - contains
+ * 7. xPath
+ * 8. cssSelector
+ * 
+ * There will be multiple input tags - So, Selenium when it finds multiple matched tag, it takes the first match
+ * 
+ * To navigate back - driver.navigate().back();
+ * To navigate forward - driver.navigate().forward();
+ * To refresh - driver.navigate().refresh();
+ * 
+ */
+
+
+		
+
+		
+
+

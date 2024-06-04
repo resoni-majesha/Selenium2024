aa9c5d3263c7444b2b59bd8f84a7afe74eca5299
resoni-majesha <77217620+resoni-majesha@users.noreply.github.com>
2024-06-04 12:59:29 +0530

    Add files via upload

---
+++ day1224/ChromeBrowser.java
@@ -0,0 +1,33 @@
+package week1.day1224;
+
+public class ChromeBrowser {
+	
+	public String launchBrowser(String browserName) {
+		
+		System.out.println("Browser launched successfully");
+		return browserName;
+	}
+	
+	public void loadUrl() {
+		
+		System.out.println("Application launched successfully");
+		
+	}
+
+	public static void main(String[] args) {
+		
+		ChromeBrowser obj = new ChromeBrowser();
+		
+		String launchBrowser = obj.launchBrowser("Chrome");
+		System.out.println(launchBrowser);
+		obj.loadUrl();
+
+	}
+
+}
+
+/*
+Browser launched successfully
+Chrome
+Application launched successfully
+*/

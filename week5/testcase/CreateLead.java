3529a6ffa6d54030240b15558d2cfed29d9a801e
resoni-majesha <77217620+resoni-majesha@users.noreply.github.com>
2024-06-20 17:02:01 +0530

    Add files via upload

---
+++ week5/testcase/CreateLead.java
@@ -0,0 +1,33 @@
+package week5.testcase;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CreateLead {
+
+	public static void main(String[] args) {
+		
+        ChromeDriver driver = new ChromeDriver(); // Launch the browser
+		
+		driver.get("http://leaftaps.com/opentaps/control/main"); // Get the URL
+		
+		driver.manage().window().maximize(); // Maximize the window
+		
+		driver.findElement(By.id("username")).sendKeys("demosalesmanager");
+		// Enter username & password
+		
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		
+		driver.findElement(By.className("decorativeSubmit")).click();
+		// Click on login
+		
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		// Click on CRM/SFA link
+		
+		driver.findElement(By.linkText("Leads")).click();
+		// Click on leads tab
+		
+        driver.findElement(By.linkText("Create Lead")).click();
+	}
+
+}

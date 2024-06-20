f3dff5ce457762f4effb929a3110564c79676e06
resoni-majesha <77217620+resoni-majesha@users.noreply.github.com>
2024-06-20 16:56:56 +0530

    Delete day3221 directory

--- day3221/Mother.java
+++
@@ -1,9 +0,0 @@
-package week3.day3221;
-
-public interface Mother {
-	
-	public void stopSmoke();
-	
-	public void moreAffection();
-
-}
--- day3221/Wife.java
+++
@@ -1,9 +0,0 @@
-package week3.day3221;
-
-public interface Wife {
-	
-	public void stopSmoke();
-	
-	public void scold();
-
-}

69fec319421462660c37d295914dbf879f2533d4
resoni-majesha <77217620+resoni-majesha@users.noreply.github.com>
2024-06-27 00:40:38 +0530

    Add files via upload

--- steps/CreateLead.java
+++ steps/CreateLead.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.cucumber.java.en.When;
+import io.cucumber.testng.CucumberOptions;
 
 public class CreateLead extends BaseClass {
 
@@ -25,6 +26,32 @@ public class CreateLead extends BaseClass {
 * Here driver is not shared, we should share driver across all the java files
 * So create a base class in steps folder to share the driver
 * Move the driver declaration in the BaseClass - public ChromeDriver driver;
+* Make it as static, public static ChromeDriver driver; -> If it is static it will be having only one copy, not multiple copies
+* So whenever the driver is getting initialized in LeafTapsLogin class, it will update the global variable
+* i.e., the session id which is created in driver = new ChromeDriver();, that will get updated in the global variable
+* So the CreateLead which is referring the driver from the global variable, that will get the session ID
+* static is necessary if the method is distributed across multiple java classes, then yu should declare the driver as static
+* If yu dont declare the driver as static then the driver which is initialized in LeafTapsLogin won't be shared to CreateLeadclass
+* If it is not declared as static, we will get NullPointerException as the driver was null
+* If we make that as static, whenever the session id is getting generated, it will get updated in the global variable
+* 
 * Now all the class in steps should extends BaseClass
+* CreateLead extends BaseClass
+* LeafTapsLogin extends BaseClass
+* 
+* Create the implementation for unimplemented steps
+* When Click on crmsfa link
+* And Click on Leads link
+* 
+* Now, we have background in each feature file, BLogin.feature, CreateLead
+* If yu want to execute both the feature files, go o runner class, RunLogin, 
+* @CucumberOptions(features = "src/test/java/features/DLogin.feature", glue = "steps")
+* Here features is plural, so it will accept array of values, 
+* Add the location for CreateLead.feature file in @CucumberOPtions, inside curly braces
+* @CucumberOptions(features = {"src/test/java/features/BLogin.feature", "src/test/java/features/CreateLead.feature"}, glue = "steps")
+* This is one method
+* 
+* The other way is give the package name itself
+* @CucumberOptions(features = {"src/test/java/features}, glue = "steps"), s it will execute all the files in this package
 *
 */

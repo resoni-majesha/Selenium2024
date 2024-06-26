2966f02931cbbd55aeda7110dfb376bed7c51155
resoni-majesha <77217620+resoni-majesha@users.noreply.github.com>
2024-06-27 01:52:49 +0530

    Add files via upload

--- runner/RunLogin.java
+++ runner/RunLogin.java
@@ -3,7 +3,9 @@ package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = {"src/test/java/features/BLogin.feature", "src/test/java/features/CreateLead.feature"}, glue = "steps")
+@CucumberOptions(features = {"src/test/java/features/BLogin.feature", "src/test/java/features/CreateLead.feature"}, 
+                 glue = "steps",
+                 publish = true)
 // @CucumberOptions(features = {"src/test/java/features"}, glue = "steps")
 public class RunLogin extends AbstractTestNGCucumberTests {
 
@@ -32,4 +34,14 @@ public class RunLogin extends AbstractTestNGCucumberTests {
 * We need to give information about step definition also
 * Add comma after features in @CucumberOptions() annotation and add the attribute called glue and yu have to give the package name
 * glue = "steps" -> @CucumberOptions(features = "src/test/java/features/Login.feature", glue = "steps")
+* 
+* If yu want to display the results in console, use an attribute called publish = true
+* @CucumberOptions(features = {"src/test/java/features/BLogin.feature", "src/test/java/features/CreateLead.feature"}, 
+                 glue = "steps",
+                 publish = true)
+* 
+* Yu will get a html report -> https://reports.cucumber.io/reports/41f776f3-6ff3-4b0a-96c9-5636a032117b
+* Copy and paste it in the browser -> CLick on the sublinks and view the results for each testcase
+* 
+* In real time, we are not going to use TestNG report, Cucumner report, we will use extend report from our final Framework
 */
